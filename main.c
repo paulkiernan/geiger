a06eb2e4310d5416ad22379bd0ff63450e2c5178
Paul Kiernan <paul@chartbeat.com>
2015-05-28 13:24:53 -0400

    working measurements

--- main.c
+++ main.c
@@ -24,14 +24,16 @@ FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 #define t1 250
 
 // task subroutines
-void task1(void);       //blink at 2 or 8 Hz
+void led_heartbeat(void);       //blink at 2 or 8 Hz
 void initialize(void);  //all the usual mcu stuff
 void init_lcd(void);    //initalize the LCD
+uint16_t  ReadADC(uint8_t);    //initalize the LCD
 
-int rad_counter;
+long Ain;                       // raw A to D number
+int rad_counter = 0;            // CPM
 int8_t lcd_buffer[17];          // LCD display buffer
-volatile unsigned char time1;   //timeout counter
-unsigned char led;              //light states
+volatile unsigned char time1;   // timeout counter
+unsigned char led;              // light states
 const int8_t LCD_initialize[] PROGMEM = "LCD Initialized\0";
 const int8_t LCD_chartbeat[] PROGMEM = "CHARTBEAT    CPM\0"; // LCD init msg
 const int8_t LCD_hackweek[]  PROGMEM = "HACKWEEK!\0";  // LCD init msg
@@ -54,34 +56,62 @@ ISR (INT0_vect){
 //Entry point and task scheduler loop
 int main(void){
     initialize();
-    init_lcd();
-
-    // init the UART -- uart_init() is in uart.c
-    uart_init();
+    uart_init();    // init the UART -- uart_init() is in uart.c
     stdout = stdin = stderr = &uart_str;
+    fprintf(stdout, "\033[2J");
+    fprintf(stdout, "##########################\n\r");
     fprintf(stdout, "Starting Geiger Counter...\n\r");
+    fprintf(stdout, "##########################\n\r");
 
-    rad_counter = 0;
+    init_lcd();     // init the LCD screen
 
     //main task scheduler loop
     int temp_clicks = 0;
     while(1){
         if (time1==0){
             time1=t1;
-            task1();
+            led_heartbeat();
         }
         if (temp_clicks != rad_counter){
-            sprintf(lcd_buffer,"Clicks: %i", rad_counter);
+            sprintf(lcd_buffer, "SClicks: %i", rad_counter);
+            fprintf(stdout, "FClicks: %i\n\r", rad_counter);
             LCDGotoXY(0, 1);
             LCDstring(lcd_buffer, strlen(lcd_buffer));
             temp_clicks = rad_counter;
         }
+        //get the sample
+        int read_channel = 0;
+        Ain = ReadADC(read_channel);
+
+        sprintf(lcd_buffer, "SAin: %ld", Ain);
+        fprintf(stdout, "FAin: %ld\n\r", Ain);
+        /*LCDGotoXY(0, 1);*/
+        /*LCDstring(lcd_buffer, strlen(lcd_buffer));*/
+        /*_delay_ms(50);*/
     }
+}
+
+uint16_t ReadADC(uint8_t ADCchannel){
+    long result;
+
+    //select ADC channel with safety mask
+    ADMUX = (ADMUX & 0xF0) | (ADCchannel & 0x0F);
+    //single conversion mode
+    ADCSRA |= (1<<ADSC);
+
+    // wait until ADC conversion is complete
+    while( ADCSRA & (1<<ADSC) );
+
+    result = ADCL;
+    result |= ADCH<<8;
+    result = 1126400L / result; // Back-calculate AVcc in mV
+
+    return result;
 }
 
 //**********************************************************
 //Task 1
-void task1(void){
+void led_heartbeat(void){
     //toggle the second bit
     led = led ^ 1 ;
     PORTD = (led<<PORTD2) ;
@@ -92,8 +122,12 @@ void task1(void){
 //Set it all up
 void initialize(void){
 
+    // Select Vref=AVcc
+    ADMUX |= (1<<REFS0);
+    //set prescaller to 128 and enable ADC
+    ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
+
     DDRD = (1<<PORTD2);    // PORT D.2 is an output
-    DDRD &= ~(1<<PORTD0);  // PORT D.0 is an input
 
     //set up timer 0 for 1 mSec timebase
     TIMSK0= (1<<OCIE0A);    //turn on timer 0 cmp match ISR
@@ -116,16 +150,6 @@ void initialize(void){
     sei();
 }
 
-
-// Initalize the LCD stuff
-/*void init_lcd(void){*/
-    /*_delay_ms(500);*/
-    /*LCDinit();          //initialize the display*/
-    /*LCDcursorOFF();     //turn off the LCD cursor*/
-    /*LCDclr();           //clear the display*/
-    /*LCDGotoXY(0,0);*/
-/*}*/
-
 void init_lcd(void){
     _delay_ms(500);
     LCDinit();   //initialize the display
@@ -139,5 +163,4 @@ void init_lcd(void){
     LCDGotoXY(0,0);
     CopyStringtoLCD(LCD_chartbeat, 0, 0);
     CopyStringtoLCD(LCD_hackweek, 0, 1);
-
 }
